53884e0b3cafac6a6d558f2b1dec90fbbe9f94ba
jovicenko <jovicenkoo@gmail.com>
2022-07-04 21:17:44 +0200

    Components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { SingleQuoteComponent } from './components/single-quote/single-quote.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    SingleQuoteComponent,
+    LoginComponent,
+    RegisterComponent 
   ],
   imports: [
     BrowserModule,
